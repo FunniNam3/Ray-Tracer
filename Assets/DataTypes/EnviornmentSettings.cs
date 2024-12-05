624d3acdf6b81e9e5e7f0a16cefbef1caaba04ac
FunniNam3 <nguyen.ev03@gmail.com>
2024-12-05 02:56:30 -0600

    Added Enviornment settings

---
+++ Assets/DataTypes/EnviornmentSettings.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+[System.Serializable]
+public struct EnviornmentSettings
+{
+    public Color SkyColorHorizon;
+    public Color SkyColorZenith;
+    public Vector3 SunLightDirection;
+    public float SunFocus;
+    public float SunIntensity;
+    public Color GroundColor;
+}
--- Assets/Scripts/RayTracingManager.cs
+++ Assets/Scripts/RayTracingManager.cs
@@ -7,12 +7,14 @@ public class RayTracingManager : MonoBehaviour
 {
     [Header("Ray Tracing Settings")]
     [SerializeField, Range(0, 32)] int MaxBounceCount = 1;
+    [SerializeField, Range(0, 64)] int NumRaysPerPixel = 1;
+    [SerializeField] EnviornmentSettings enviornmentSettings;
 
     [Header("View Settings")]
     [SerializeField] bool useShaderInSceneView;
     [Header("Refs")]
     [SerializeField] ComputeShader computeShader;
-    public RenderTexture renderTexture;
+    public RenderTexture currRenderTexture;
 
     // Buffers
     private ComputeBuffer sphereBuffer;
@@ -24,28 +26,31 @@ public class RayTracingManager : MonoBehaviour
 
         if (useShaderInSceneView)
         {
-            if (renderTexture == null || renderTexture.width != src.width || renderTexture.height != src.height || renderTexture.format != src.format)
+            if (currRenderTexture == null || currRenderTexture.width != src.width || currRenderTexture.height != src.height || currRenderTexture.format != src.format)
             {
-                if (renderTexture != null) renderTexture.Release();
-                renderTexture = new RenderTexture(src.width, src.height, 24, src.format);
-                renderTexture.enableRandomWrite = true;
-                renderTexture.Create();
+                if (currRenderTexture != null) currRenderTexture.Release();
+                currRenderTexture = new RenderTexture(src.width, src.height, 24, src.format);
+                currRenderTexture.enableRandomWrite = true;
+                currRenderTexture.Create();
             }
 
             int frameCount = Time.frameCount;
             computeShader.SetInt("FrameCount", frameCount);
             computeShader.SetInt("MaxBounceCount", MaxBounceCount);
+            computeShader.SetInt("NumRaysPerPixel", NumRaysPerPixel);
 
-            computeShader.SetTexture(0, "Result", renderTexture);
+            UpdateEnviornmentParms();
+
+            computeShader.SetTexture(0, "Result", currRenderTexture);
             UpdateCameraParms(cam);
             CreateSpheres();
 
-            int threadGroupsX = Mathf.CeilToInt(renderTexture.width / 8.0f);
-            int threadGroupsY = Mathf.CeilToInt(renderTexture.height / 8.0f);
+            int threadGroupsX = Mathf.CeilToInt(currRenderTexture.width / 8.0f);
+            int threadGroupsY = Mathf.CeilToInt(currRenderTexture.height / 8.0f);
 
             computeShader.Dispatch(0, threadGroupsX, threadGroupsY, 1);
-            Graphics.Blit(renderTexture, dest);
-            renderTexture.Release();
+            Graphics.Blit(currRenderTexture, dest);
+            currRenderTexture.Release();
         }
         else
         {
@@ -60,6 +65,16 @@ public class RayTracingManager : MonoBehaviour
         public Vector3 dir;
     };
 
+    void UpdateEnviornmentParms()
+    {
+        computeShader.SetVector("SkyColorHorizon", enviornmentSettings.SkyColorHorizon);
+        computeShader.SetVector("SkyColorZenith", enviornmentSettings.SkyColorZenith);
+        computeShader.SetVector("SunLightDirection", enviornmentSettings.SunLightDirection);
+        computeShader.SetFloat("SunFocus", enviornmentSettings.SunFocus);
+        computeShader.SetFloat("SunIntensity", enviornmentSettings.SunIntensity);
+        computeShader.SetVector("GroundColor", enviornmentSettings.GroundColor);
+    }
+
     void UpdateCameraParms(Camera cam)
     {
         float planeHeight = 2.0f * cam.nearClipPlane * Tan(cam.fieldOfView * 0.5f * Deg2Rad);
@@ -68,7 +83,7 @@ public class RayTracingManager : MonoBehaviour
         computeShader.SetVector("ViewParams", new Vector3(planeWidth, planeHeight, cam.nearClipPlane));
         computeShader.SetMatrix("CamLocalToWorldMatrix", cam.transform.localToWorldMatrix);
         computeShader.SetVector("WorldSpaceCameraPos", cam.transform.position);
-        computeShader.SetVector("Resolution", new Vector2(renderTexture.width, renderTexture.height));
+        computeShader.SetVector("Resolution", new Vector2(currRenderTexture.width, currRenderTexture.height));
     }
 
     void CreateSpheres()
@@ -104,7 +119,7 @@ public class RayTracingManager : MonoBehaviour
 
     void OnDestroy()
     {
-        if (renderTexture != null) renderTexture.Release();
+        if (currRenderTexture != null) currRenderTexture.Release();
         if (sphereBuffer != null) sphereBuffer.Release();
     }
 
